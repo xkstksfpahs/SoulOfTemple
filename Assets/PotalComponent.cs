161601015e0b2a919e06a20e48d49c4dd2db5bcb
xkstksfpahs <heis2514@gmail.com>
2022-10-15 16:21:59 +0900

    22/10/15 짧은작업
    이전 스테이지를 클리어하지 않으면 다음 스테이지 잠김기능 추가
    
    타이틀UI 옵션창에서도 화면스케일 조정 가능하도록 기능추가

--- Assets/PotalComponent.cs
+++ Assets/PotalComponent.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PotalComponent : MonoBehaviour
 {
+    GameSetComponent gsc;
+    string stage;
+    private void Start()
+    {
+        gsc = GameObject.Find("GameSetUI").GetComponent<GameSetComponent>();
+        stage = "Stage" + gsc.stageNumber.ToString() + "Clear";
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameManager.gm.GameSet(true);
+            PlayerPrefs.SetInt(stage, 1);
         }
     }
 }
--- Assets/buttonObject.cs
+++ Assets/buttonObject.cs
@@ -8,16 +8,32 @@ public class buttonObject : MonoBehaviour
     SpriteRenderer sr;
     public int stageNum;
     StageGameManager sgm;
+    [SerializeField] GameObject UnlockedSprite;
+    bool isLocked;
+    string beforeStage;
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.SetInt("Stage0Clear", 1);
         sr = GetComponent<SpriteRenderer>();
         sgm = GameObject.Find("StageGameManager").GetComponent<StageGameManager>();
+        beforeStage = "Stage" + (stageNum - 1).ToString() + "Clear";
+        if(PlayerPrefs.GetInt(beforeStage) == 1) // 이전 스테이지를 클리어 했을 때
+        {
+            isLocked = false;
+            UnlockedSprite.SetActive(false);
+        }
+        else if (PlayerPrefs.GetInt(beforeStage) == 0)// 이전 스테이지를 클리어 하지 않았을 때
+        {
+            isLocked = true;
+            UnlockedSprite.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isLocked == true) return;
         if(Input.GetMouseButton(0) && isClick)
         {
             sr.color = new Color(0.5f, 0.5f, 0.5f, 1);
