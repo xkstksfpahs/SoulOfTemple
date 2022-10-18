926a3cc3d81fa9f7d9abe879162286cbab026e0a
xkstksfpahs <heis2514@gmail.com>
2022-10-19 00:01:46 +0900

    22/10/19 짧작업
    타이틀 화면에 움직이는데 쓰일 템플 이미지 추가
    
    템플이미지 샘플용 Move스크립트 추가

---
+++ Assets/MovingImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingImage : MonoBehaviour
+{
+    [SerializeField] Image TempleImage;
+
+    [SerializeField] GameObject[] TempleAtoB;
+
+
+    float TempleSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TempleImage.transform.position = TempleAtoB[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TempleImageMove();
+    }
+
+    void TempleImageMove()
+    {
+        if (TempleImage.transform.position.x > TempleAtoB[1].transform.position.x)
+        {
+            TempleImage.transform.Translate(Vector2.left * TempleSpeed);
+        }
+
+        if(TempleImage.transform.position.x <= TempleAtoB[1].transform.position.x)
+        {
+            TempleImage.transform.position = TempleAtoB[0].transform.position;
+        }
+    }
+}
